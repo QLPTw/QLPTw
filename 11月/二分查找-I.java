1c95f24defefcb8311283d73db743144d19d8630
QLPTw <137791802+QLPTw@users.noreply.github.com>
2023-11-05 23:05:20 +0800

    Add files via upload

---
+++ 11月/二分查找-I.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param nums int整型一维数组 
+     * @param target int整型 
+     * @return int整型
+     */
+    public int search (int[] nums, int target) {
+        // write code here
+        int left=0,right=nums.length-1;
+        int mid;
+        if(nums.length==0){
+            return -1;
+        }
+        while(left<=right){
+            mid=(left+right)/2;
+            if(nums[mid]<target){
+                left=mid+1;
+            }else if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
