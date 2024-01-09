4ea17020df107275c8bf011f4c58a26cabb4f02e
QLPTw <137791802+QLPTw@users.noreply.github.com>
2024-01-09 20:48:06 +0800

    Add files via upload

---
+++ 24-1/牛群排列的最大深度(三行搞定).java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/*
+ * public class TreeNode {
+ *   int val = 0;
+ *   TreeNode left = null;
+ *   TreeNode right = null;
+ *   public TreeNode(int val) {
+ *     this.val = val;
+ *   }
+ * }
+ */
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param root TreeNode类 
+     * @return int整型
+     */
+    public int maxDepth (TreeNode root) {
+        // write code here
+        if(root==null){
+            return 0;
+        }
+        return Math.max(maxDepth(root.left)+1,maxDepth(root.right)+1);
+    }
+}
