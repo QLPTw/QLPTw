c72c06bd88d1d4ecaa3b2f20b02a4aee7b372b25
QLPTw <137791802+QLPTw@users.noreply.github.com>
2024-01-14 21:51:36 +0800

    Add files via upload

---
+++ 24-1/统计农场牛数量.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+/*
+ * public class TreeNode {
+ *   int val = 0;
+ *   TreeNode left = null;
+ *   TreeNode right = null;
+ *   public TreeNode(int val) {
+ *     this.val = val;
+ *   }
+ * }
+ */
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param root TreeNode类
+     * @return int整型
+     */
+    public int countNodes (TreeNode root) {
+        // write code here
+        if (root == null) {
+            return 0;
+        }
+        int leftCount = countNodes(root.left);
+        int rightCount = countNodes(root.right);
+        return leftCount + rightCount + 1; 
+    }
+
+}
